5be1481475aa297bfc9fed863e0edd97486a8f62
Sumanth <va20070770@wipro.com>
2019-06-25 21:49:57 +0530

    Intial Commit

---
+++ src/test/java/com/eureka/server/poc/eurekaserverpoc/EurekaServerPocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eureka.server.poc.eurekaserverpoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
